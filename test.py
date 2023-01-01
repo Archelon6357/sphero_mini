17bb43eeba345d46cad25733f68b2b90cff421c4
Archelon6357 <archelon.genbu.6357@gmail.com>
2023-01-01 17:50:33 +0900

    upload

--- test.py
+++ test.py
@@ -1,27 +1,35 @@
 import asyncio
 from bleak import BleakClient
 
-from const import *
+from sphero_const   import *
+from sphero_uuid    import *
 
 address = "E7:A9:B0:C2:CD:7F"
-
-MODEL_NBR_UUID = "00002a00-0000-1000-8000-00805f9b34fb"
-
-UUID_SPHERO_CHARACTERISTIC_HANDLE_1C = "00010002-574f-4f20-5370-6865726f2121"
-UUID_SPHERO_CHARACTERISTIC_ANTI_DOS = "00020005-574f-4f20-5370-6865726f2121"
 
 # async def main(address):
 #     async with BleakClient(address) as client:
 #         model_number = await client.read_gatt_char(MODEL_NBR_UUID)
 #         print("Model Number: {0}".format("".join(map(chr, model_number))))
 
-
 async def main(address):
-    async with BleakClient(address) as client:
+
+    disconnected_event = asyncio.Event()
+
+    def disconnected_callback(client):
+        print("Disconnected callback called!")
+        disconnected_event.set()
+
+    async with BleakClient(address, disconnected_callback = disconnected_callback) as client:
         
+        # 接続を10秒後に切断する機能をOFFにする
         await client.write_gatt_char(UUID_SPHERO_CHARACTERISTIC_ANTI_DOS ,"usetheforce...band".encode(), response=True)
 
-        sendBytes = [0x8d, sum([0x02, 0x08]), 0x13, 0x0D, 1]
+
+        # sphero mini を 起こす．
+        sendBytes = [sendPacketConstants["StartOfPacket"],
+            sum([flags["requestsResponse"], flags["resetsInactivityTimeout"]]), 
+            deviceID["powerInfo"], 
+            powerCommandIDs["wake"], 1]
 
         checksum = 0
         for num in sendBytes[1:]:
@@ -34,6 +42,30 @@ async def main(address):
 
         await client.write_gatt_char(UUID_SPHERO_CHARACTERISTIC_HANDLE_1C, output, response = True)
 
+        # 2秒待機
+        await asyncio.sleep(2.0)
+
+        # sphero mini を DeepSleep mode にする．
+        sendBytes = [sendPacketConstants["StartOfPacket"],
+                    sum([flags["requestsResponse"], flags["resetsInactivityTimeout"]]), 
+                    deviceID["powerInfo"], 
+                    powerCommandIDs["sleep"], 1]
+
+        checksum = 0
+        for num in sendBytes[1:]:
+            checksum = (checksum + num) & 0xFF # bitwise "and to get modulo 256 sum of appropriate bytes
+        checksum = 0xff - checksum # bitwise 'not' to invert checksum bits
+        sendBytes += [checksum, 0xd8] # concatenate
+
+        output = b"".join([x.to_bytes(1, byteorder='big') for x in sendBytes])
+        print(output)         
+
+        await client.write_gatt_char(UUID_SPHERO_CHARACTERISTIC_HANDLE_1C, output, response = True)        
+
+        # sendBytes = [sendPacketConstants["StartOfPacket"],
+        #      sum([flags["requestsResponse"], flags["resetsInactivityTimeout"]]), 
+        #      deviceID["powerInfo"], 
+        #      powerCommandIDs["wake"], 1]
 
         # self._send(characteristic=self.API_V2_characteristic,
         #     devID=deviceID['powerInfo'],
@@ -63,4 +95,5 @@ async def main(address):
 
 
 asyncio.run(main(address))
+
 
